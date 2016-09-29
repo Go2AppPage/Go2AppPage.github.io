4f35d73857d6270a5375aa395d8e67e09df516c3
Viral Patel <viral.y.patel@gmail.com>
2016-09-29 17:28:18 +0530

    detect query string and build link as - index.html#?p=com.whatsapp

--- app/app-stores/app-stores.component.js
+++ app/app-stores/app-stores.component.js
@@ -5,19 +5,26 @@ angular.
   module('appStores').
   component('appStores', {
     templateUrl: 'app-stores/app-stores.template.html',
-    controller: function AppStoresController() {
+    controller: function AppStoresController($location) {
+
+        var appid = $location.search().id;
+
         this.stores = [
           {
             name: 'Google Play Store',
+            url: 'https://play.google.com/store/apps/details?id=' + appid,
             img: 'img/Google_Play_Store.png'
           }, {
             name: 'Amazon App Store',
+            url: 'https://play.google.com/store/apps/details?id=' + appid,
             img: 'img/amazon_app_store.png'
           }, {
             name: 'Samsung App Store',
+            url: 'https://play.google.com/store/apps/details?id=' + appid,
             img: 'img/samsung_app_store.png'
           }, {
             name: 'SlideMe App Store',
+            url: 'https://play.google.com/store/apps/details?id=' + appid,
             img: 'img/slideme_app_store.png'
           }
         ];
